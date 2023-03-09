896823deaf7e845eab229da6320483948c2b4808
Aleksandr <8712060@gmail.com>
2023-03-09 19:27:41 +0300

    lession_5

---
+++ todo/frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer mt-auto py-3">
+      <div className="container">
+        <span className="text-muted">GeekBrains 2023 group GU_python_2388 </span>
+      </div>
+    </footer>
+  );
+}
